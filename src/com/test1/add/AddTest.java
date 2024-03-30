b05afe0b406bbfaa7f20a5801ccfdb2c80aa7228
YZL12572 <a18822141980@163.com>
2024-03-30 17:21:29 +0800

    这是我第一次提交！！！！！

---
+++ src/com/test1/add/AddTest.java
@@ -0,0 +1,14 @@
+package com.test1.add;
+
+/**
+ * @Auther: 杨振龙
+ * @Date: 2024/3/30 - 03 - 30 - 17:18
+ * @Description: com.test1.add
+ * @version: 1.0
+ */
+public class AddTest {
+    public static void main(String[] args) {
+        //这是一个main方法，是程序的入口
+
+    }
+}
